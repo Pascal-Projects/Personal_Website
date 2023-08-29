7c9a9c1d5c767c731758a5c3b54a2a77624532b2
Pascal <pascal13yt@gmail.com>
2023-08-29 19:36:11 +0200

    Moved Files to public directory

--- js/useragent.js
+++
@@ -1,3 +0,0 @@
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    alert("The support for smart devices is currentlz under development. \n You can view the site after enabling desktopmode");
-}
---
+++ public/js/useragent.js
@@ -0,0 +1,3 @@
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    alert("The support for smart devices is currentlz under development. \n You can view the site after enabling desktopmode");
+}
