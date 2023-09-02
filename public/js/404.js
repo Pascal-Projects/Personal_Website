24f7dfe2702b468f6c3d5ed0f5ed95eb0f417c77
Pascal <pascal13yt@gmail.com>
2023-09-02 21:08:10 +0200

    Fixed DRP functions

--- public/js/404.js
+++ public/js/404.js
@@ -1,6 +1,5 @@
 window.onload = function () {
     let image = document.getElementById("discord");
-    let src = image.src;
 
     function updateImage() {
         //image.src = image.src.split("?")[0] + "?" + new Date().getTime() + "idleMessage=Currently%20chillin...&bg=474d58&showDisplayName=true&hideBadges=true";
