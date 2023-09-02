906f08736fa988cb6321277f1c0a4bb2331218d7
Pascal <pascal13yt@gmail.com>
2023-09-02 21:05:57 +0200

    Prefixed and Fromatted all files

--- public/js/discordUpdate.js
+++ public/js/discordUpdate.js
@@ -1,8 +1,8 @@
-window.onload = function (){
+window.onload = function () {
     let image = document.getElementById("discord");
     let src = image.src;
 
-    function updateImage(){
+    function updateImage() {
         image.src = image.src.split("?")[0] + "?" + new Date().getTime() + "?idleMessage=Currently%20chillin...&bg=474d58&showDisplayName=true&hideBadges=true";
     }
 
