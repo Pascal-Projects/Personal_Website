69255970879df105063fe8c507dca56774ceb09b
Pascal <pascal13yt@gmail.com>
2024-05-10 21:28:26 +0200

    Small update to the discord RPC

--- public/js/discordUpdate.js
+++ public/js/discordUpdate.js
@@ -2,7 +2,7 @@ window.onload = function () {
     let image = document.getElementById("discord");
 
     function updateImage() {
-        image.src = image.src.split("?")[0] + "?" + "bg=474d58&showDisplayName=true&hideBadges=true&idleMessage=Currently%20chillin...&" + new Date().getTime();
+        image.src = image.src.split("?")[0] + "?" + "bg=474d58&showDisplayName=true&hideBadges=true&hideActivity=whenNotUsed&" + new Date().getTime();
     }
 
     setInterval(updateImage, 500)
